f41950b2b6582d64005ec391801ce1b0af866c6c
Aymerick <mricmricmric@gmail.com>
2020-07-22 16:15:27 +0200

    A

--- README.md
+++ README.md
@@ -1,12 +1,48 @@
 # Stage_Aymerick
 
-Config : Parameters of each device
+FILE IN THIS FOLDERS:
 
-Driver : Drivers for the 3D-mill
+--------------------
 
-log_file : Historic of instruction send to the 3d-mill
+Config : Parameters of each device (Printer,Wells,Valve). Read it before using any program to be sure that the parameters correspond to your devices. You can change their value but don't change the name of the variable. You can add new variable if you need to. To call a variable in an other script you have to:
+	import Config (if your script is in the same deposit as the file Config.py)
+	write your variable like this : Config.$NameOfYourVariable"
 
-Puit : Program to move the 3D-mill to specific coordinates
+----------------------------------------------------
 
-Valve : Program to control the valve
+Driver : Drivers for the 3D-mill.Contains all the methods to interact with the device. They are basic functions used in the script "Wells.py".
+Methods in Driver :
+	-Open/Close Connection
+	-Status
+	-WaitForIdle
+	-Move
+If you want further informations about the methods check the file himself
+
+----------------------------------------------------
+
+log_file : Historic of instruction send to the 3d-mill. If the printer seems to be not working when u send a list of instruction, chek the file to be sure there is not a problem with your script. For example if u open two times the communication with the device.
+
+----------------------------------------------------
+
+Wells : Program to move the 3D-mill to specific wells. This script use the class CNC created in Driver to move the 3d-mill.
+Methods in Wells:
+	-Wells_1
+	-CoordWells
+	-MoveWells
+	-CheckValue
+If you want further informations about the methods check the file himself 
+
+----------------------------------------------------
+
+Valve : Contains all the methods to interact with one or more valve.
+Methods in Valve:
+	-Open Connection
+	-Status
+	-WaitForIdle
+	-ValveRotation
+If you want further informations about the methods check the file himself
+
+----------------------------------------------------
+
+Problem : File where you can find some solutions to some problems I have encountered during the developement of this scripts
 
--- cnc-python/Python Program/Valve.py
+++ cnc-python/Python Program/Valve.py
@@ -4,7 +4,11 @@
 Created on Thu Jul 16 16:21:53 2020
 
 @author: aymerick
-encoding='ascii' ?
+
+The Valve class is used to control a Serial MVP instrument. The 
+following methods are the most basic functions that are neede to interact with
+the device.
+
 """
 
 import serial
@@ -13,78 +17,155 @@ import Config
 
 class Valve:
     def __init__(self):
-        self.port=Config.PortValve
-        self.ValveState={}
-        self.ValveState["ValvePosition"]=1
-        self.ValveState["Outrange"]="Values within limits"
-        Valve.OpenConnection(self)
-        self.NbValve=0
+        """
+        For the class instanciation, the input variable port is indicating which 
+        COM port the valve is connected to.
+
+        Parameters
+        ----------
+        None.
+
+        Returns
+        -------
+        None.
+
+        """      
+        self.port=Config.PortValve                            # Save the name of the port given in the "file Config.py" in "self.port"
+        self.ValveState={}                                    # Create a dictionnary
         
     def OpenConnection(self):
-        self.s = serial.Serial(self.port,baudrate=9600,bytesize=serial.SEVENBITS,parity=serial.PARITY_ODD,stopbits=serial.STOPBITS_ONE);
-        time.sleep(2)
-        self.s.flushInput()
-        Init="1a\r"
-        self.s.write(Init.encode())
-        time.sleep(0.1)
-        string2Send="aLXR\r"
-        self.s.write(string2Send.encode())
-        Valve.WaitForIdle(self)
+        """
+        The OpenConnection method is opening the serial communication port and 
+        keeps the output in self.
+        The time delay of 2s is important to keep, else the communication will 
+        not be established and it won't be possible to control the valve.
+
+        Returns
+        -------
+        None.
+
+        """
+        self.s = serial.Serial(self.port,baudrate=9600,bytesize=serial.SEVENBITS,parity=serial.PARITY_ODD,stopbits=serial.STOPBITS_ONE) # Open the port and keep the output in self.s
+        time.sleep(2)                                                  # Wait 2 seconds
+        self.s.flushInput()                                            # Remove data from input buffer
+        Init="1a\r"                                                    # This str input ask the device to do an auto adressing (First valve="a"; Second valve="b"; ....)
+        self.s.write(Init.encode())                                    # Send the command to the device encoded in UTF-8
+        for i in range (0,Config.NbofValve):                           # We want to initialize every valve adressed 
+            string2Send=Config.ValveNB[i]+"LXR\r"                      # This str input initialize the current valve (Config.ValveNB[0]='a';Config.ValveNB[1]='b')
+            self.s.write(string2Send.encode())                         # Send the command to the device encoded in UTF-8
+            Valve.WaitForIdle(self)                                    # Use the method WaitForIdle to be sure the initialization is finished before sending new instructions
+            self.ValveState["ValvePosition"+str(Config.ValveNB[i])]=1  # Save the position of each valve (After initialization every valve shall be in position 1)          
+            self.ValveState["Outrange"+str(Config.ValveNB[i])]="Values within limits"  # For now all the values are in the limits
     
     def Status(self):
-        self.s.flushInput()
-        status="aF\r"
-        self.s.write(status.encode())
-        Line=self.s.read()
-        grbl_out=self.s.read()
-        self.ValveState["State"]=grbl_out.decode()
+        """
+        The Status method is checking if the one of the valve is doing something or not
+
+        Returns
+        -------
+        None.
+
+        """
+        self.s.flushInput()                                             # Remove data from input buffer
+        for i in range (0,Config.NbofValve):                            # Check the state of each valve    
+            status=Config.ValveNB[i]+"F\r"                              # This str input ask to the current valve his state
+            self.s.write(status.encode())                               # Send the command to the device encoded in UTF-8 
+            Line1=self.s.read()                                         # First live give us useless information so we will not use it
+            Output=self.s.read()                                        # Return "*" if the valve is busy "Y" if the valve is idle
+            self.ValveState["State"+Config.ValveNB[i]]=Output.decode()  # Save the state ofthe current valve in the dictionnary
      
     def WaitForIdle(self):
-        Valve.Status(self)
-        WaitingTime=0
-        while self.ValveState["State"]!="Y":
-            print(self.ValveState["State"])
-            time.sleep(1)
-            Valve.Status(self)
-            WaitingTime=WaitingTime+1
-            if WaitingTime >= 15.0:
-                print("There is an error")
+        """
+        The WaitForIdle method wait 1 sec if one of the valve is already working
 
-    def ValveRotation(self,MD,pp):    #MD=module adress
-        if 0>=pp or pp>=9:
+        Returns
+        -------
+        None.
+
+        """
+        Valve.Status(self)                                         # Check the status of each valve
+        WaitingTime=0
+        for i in range (0,Config.NbofValve):                       # Check if the status is idle or not for each device
+            while self.ValveState["State"+Config.ValveNB[i]]!="Y": # While the status is not "Y" (valve is idle) for the current valve, we wait 1 sec
+                time.sleep(1)                                      # We wait 1 sec
+                Valve.Status(self)                                 # Check if the status has changed
+                WaitingTime=WaitingTime+1
+                if WaitingTime >= 15.0:                            # If the status has not changed in 15 sec there might be an error so we stop the "while" and write an error message
+                    print("There is an error")
+                    break
+
+    def ValveRotation(self,MD,pp):    
+        """
+        The ValveRotation method is sending instructions to a specific valve in order to 
+        rotate the valve. The user must indicate the position of the valve as input and 
+        the name of the valve (Module adress). In order to optimize the rotation the method
+        will choose rotate clockwis or counterclockwise depending of wich one is fastest.
+
+        Parameters
+        ----------
+        MD : Module adress
+        pp : Position
+        
+        Returns
+        -------
+
+        """
+        if 0>=pp or pp>=Config.ValveConfigPosition[MD]+1:                                              # Check if the valve number is fine or not depending of the valve configuration (Look Config.py)
             self.ValveState["Outrange"]="Valve number is out of the limits"
             print("Wrong Value")
-#        elif MD!="a" and "b" and "c":
-#            self.ValveState["Outrange"]="Valve module adress is wrong"
-#            print("Wrong Address")
-        else :
-            if 1<=self.ValveState["ValvePosition"]<=4:
-                opposite=self.ValveState["ValvePosition"]+4
-            else:
-                opposite=self.ValveState["ValvePosition"]-4
-            if (pp-opposite)<=0:
-                self.ValveState[self.NbValve]=str(MD)+"LP0"+str(pp)+"R\r"
-                self.s.write(self.ValveState[self.NbValve].encode())
-                self.ValveState["ValvePosition"]=pp
-                self.NbValve=self.NbValve+1
-                Valve.WaitForIdle(self)
-            else:
-                self.ValveState[self.NbValve]=str(MD)+"LP1"+str(pp)+"R\r"
-                self.ValveState["ValvePosition"]=pp
-                self.s.write(self.ValveState[self.NbValve].encode())
-                self.NbValve=self.NbValve+1
-                Valve.WaitForIdle(self) 
+        else :                                                                                         # The idea here is to calulate the oppoite number of our current position depending of the valve configuration
+            if 1<=self.ValveState["ValvePosition"+str(MD)]<=Config.ValveConfigPosition[MD]/2:          # For example if we have 8 positions, the valve looks like this :   8  1  2
+                opposite=self.ValveState["ValvePosition"+str(MD)]+Config.ValveConfigPosition[MD]/2     #                                                                   7     3
+            else:                                                                                      #                                                                   6  5  4
+                opposite=self.ValveState["ValvePosition"+str(MD)]-Config.ValveConfigPosition[MD]/2     # So the opposite of 1 is 5; the opposite of 8 is 4; the opposite of 2 is 6; ........
+                                                                                                       # So we add 4 if our current position is lower or even at 4 and we substract 4 if our current position is 4 or upper
+                                                                                                       
+            if self.ValveState["ValvePosition"+str(MD)]<=pp<=opposite:                                 # We check if: Current position < Target position < Opposite of current position (For example : 2<3<6)
+                string2Send=str(MD)+"LP0"+str(pp)+"R\r"                                                # This str input move the valve in clockwise to target position
+                self.s.write(string2Send.encode())                                                     # Send the command to the device encoded in UTF-8
+                self.ValveState["ValvePosition"+str(MD)]=pp                                            # Change the Current position to the position where the valve will be at the end of the rotation
+                Valve.WaitForIdle(self)                                                                # Wait until the movement is finished
                 
-    def ExecuteMovement(self):
-        if self.ValveState["Outrange"]=="Values within limits":
-            for i in range (0,self.NbValve):
-                self.s.write(self.ValveState[i].encode())
-                Valve.WaitForIdle(self)
+            elif opposite<=pp<=self.ValveState["ValvePosition"+str(MD)]:                               # We check if: Opposite of current position < Target position < Current position (For example : 3<4<7)
+                string2Send=str(MD)+"LP1"+str(pp)+"R\r"                                                # This str input move the valve in counterclockwise to target position
+                self.s.write(string2Send.encode())                                                     # Send the command to the device encoded in UTF-8
+                self.ValveState["ValvePosition"+str(MD)]=pp                                            # Change the Current position to the position where the valve will be at the end of the rotation
+                Valve.WaitForIdle(self)                                                                # Wait until the movement is finished
+                
+            elif self.ValveState["ValvePosition"+str(MD)]<=opposite<=pp:                               # We check if: current position < Opposite of current position < Target position (For example : 1<5<7)
+                string2Send=str(MD)+"LP1"+str(pp)+"R\r"                                                # This str input move the valve in counterclockwise to target position
+                self.s.write(string2Send.encode())                                                     # Send the command to the device encoded in UTF-8
+                self.ValveState["ValvePosition"+str(MD)]=pp                                            # Change the Current position to the position where the valve will be at the end of the rotation
+                Valve.WaitForIdle(self)                                                                # Wait until the movement is finished
+                
+            elif opposite<=self.ValveState["ValvePosition"+str(MD)]<=pp:                               # We check if: Opposite of current position < current position < Target position (For example : 2<6<8)
+                string2Send=str(MD)+"LP0"+str(pp)+"R\r"                                                # This str input move the valve in clockwise to target position
+                self.s.write(string2Send.encode())                                                     # Send the command to the device encoded in UTF-8
+                self.ValveState["ValvePosition"+str(MD)]=pp                                            # Change the Current position to the position where the valve will be at the end of the rotation
+                Valve.WaitForIdle(self)                                                                # Wait until the movement is finished
+                
+            elif pp<=self.ValveState["ValvePosition"+str(MD)]<=opposite:                               # We check if: Target position < current position < Opposite of current position (For example : 2<3<7)
+                string2Send=str(MD)+"LP1"+str(pp)+"R\r"                                                # This str input move the valve in counterclockwise to target position
+                self.s.write(string2Send.encode())                                                     # Send the command to the device encoded in UTF-8
+                self.ValveState["ValvePosition"+str(MD)]=pp                                            # Change the Current position to the position where the valve will be at the end of the rotation
+                Valve.WaitForIdle(self)                                                                # Wait until the movement is finished
+                
+            elif pp<=opposite<=self.ValveState["ValvePosition"+str(MD)]:                               # We check if: Target position < Oppopsite of current position < current position (For example : 1<3<7)
+                string2Send=str(MD)+"LP0"+str(pp)+"R\r"                                                # This str input move the valve in clockwise to target position
+                self.s.write(string2Send.encode())                                                     # Send the command to the device encoded in UTF-8
+                self.ValveState["ValvePosition"+str(MD)]=pp                                            # Change the Current position to the position where the valve will be at the end of the rotation
+                Valve.WaitForIdle(self)                                                                # Wait until the movement is finished
+
+
 
 
 if __name__ == "__main__":
     a=Valve()
-    a.ValveRotation("a",5)
+    a.OpenConnection()
+    a.ValveRotation("a",1)
     a.ValveRotation("a",2)
+    a.ValveRotation("a",7)
+    a.ValveRotation("a",1)
+    a.ValveRotation("a",5)
     a.ValveRotation("a",6)
 
